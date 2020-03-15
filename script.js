ac0a01937cd24e3dfd100f77e247d14269eca28e
Nonsmokers <Vasyuxnevichs@mail.ru>
2020-03-16 01:49:49 +0300

    feat: add slider

--- script.js
+++ script.js
@@ -63,4 +63,33 @@ window.onscroll = function showHeader() {
         MENU[0].classList.add('menu-link-active');
       }
 }
+//slider
+let slideIndex = 1;
+showSlides(slideIndex);
 
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slider-item");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "flex";
+}
